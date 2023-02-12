9640f6d46e0213afefd5281d2ec24dc365a1c098
Harun Biswas Rubel <harunbiswasrubel@gmail.com>
2023-02-13 02:47:49 +0600

    wallet, transaction

--- wallet/index.js
+++ wallet/index.js
@@ -1,6 +1,6 @@
 const { STARTING_BALANCE } = require("../config");
 const { ec, cryptoHash } = require("../util");
-
+const Transaction = require("./transaction");
 class Wallet {
   constructor() {
     this.balance = STARTING_BALANCE;
@@ -12,6 +12,14 @@ class Wallet {
 
   sign(data) {
     return this.keyPair.sign(cryptoHash(data));
+  }
+
+  createTransaction({ recipient, amount }) {
+    if (amount > this.balance) {
+      throw new Error("Amount exceeds balance");
+    }
+
+    return new Transaction({ senderWallet: this, recipient, amount });
   }
 }
 
--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,5 +1,5 @@
 const Wallet = require("./index");
-
+const Transaction = require("./transaction");
 const { verifySignature } = require("../util");
 
 describe("Wallet", () => {
@@ -38,6 +38,41 @@ describe("Wallet", () => {
           signature: new Wallet().sign(data),
         })
       ).toBe(false);
+    });
+  });
+
+  describe("createTransaction()", () => {
+    describe("and the amount exceeds the balance", () => {
+      it("throws and error", () => {
+        expect(() =>
+          wallet.createTransaction({
+            amount: 999999,
+            recipient: "foo-recipient",
+          })
+        ).toThrow("Amount exceeds balance");
+      });
+    });
+
+    describe("and the amount is valid", () => {
+      let transaction, amount, recipient;
+
+      beforeEach(() => {
+        amount = 50;
+        recipient = "foo-recipient";
+        transaction = wallet.createTransaction({ amount, recipient });
+      });
+
+      it("create an instance of `Transaction`", () => {
+        expect(transaction instanceof Transaction).toBe(true);
+      });
+
+      it("matches the transaction input with the wallet", () => {
+        expect(transaction.input.address).toEqual(wallet.publicKey);
+      });
+
+      it("outputs the amount the recipient", () => {
+        expect(transaction.outputMap[recipient]).toEqual(amount);
+      });
     });
   });
 });
